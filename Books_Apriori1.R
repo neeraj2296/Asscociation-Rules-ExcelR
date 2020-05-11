792658440f3d057255c023a1ee871071c31d4afb
neeraj2296 <neerajkumarsj96@gmail.com>
2020-05-11 13:35:47 +0530

    Add files via upload

---
+++ Books_Apriori1.R
@@ -0,0 +1,66 @@
+library(arules)
+#install.packages("arulesViz")
+library(arulesViz)
+
+book <- read.csv(file.choose())
+book$ChildBks<-as.factor(book$ChildBks)
+book$YouthBks<-as.factor(book$YouthBks)
+book$CookBks<-as.factor(book$CookBks)
+book$DoItYBks<-as.factor(book$DoItYBks)
+book$RefBks<-as.factor(book$RefBks)
+book$ArtBks<-as.factor(book$ArtBks)
+book$GeogBks<-as.factor(book$GeogBks)
+book$ItalCook<-as.factor(book$ItalCook)
+book$ItalAtlas<-as.factor(book$ItalAtlas)
+book$ItalArt<-as.factor(book$ItalArt)
+book$Florence<-as.factor(book$Florence)
+#rules$conf
+str(book)
+
+rules = apriori(book)
+arules::inspect(rule)
+rules.sorted<-sort(rules, by = 'lift')
+arules::inspect(rules)
+
+plot(rules)
+
+summary(book)
+summary(book)
+# rules with rhs containing CookBks only
+rules = apriori(book,parameter = list(minlen = 1,supp = 0.11,conf = 0.5),appearance = list(rhs = "CookBks=1"))
+plot(rules, jitter = 0)
+
+summary(rules)
+arules::inspect(rules)
+?apriori          
+
+summary(rules)
+
+inspect(rules)
+# rules with rhs containing CookBks only
+rules = apriori(book,parameter = list(minlen = 1,supp = 0.1,conf = 0.3),appearance = list(rhs = c("CookBks=1"),lhs = c("RefBks=1","ArtBks=1","YouthBks=1","GeogBks=1","DoItYBks=1","ChildBks=1","ItalArt=1","ItalAtlas=1","ItalCook=1"), default ="none"))
+plot(rules, jitter = 0)
+#plot(rules,method='grouped')
+#plot(rules,method = 'graph',control = list(type='items'))
+summary(rules)
+arules::inspect(rules)
+?apriori          
+
+summary(rules)
+
+inspect(rules)
+
+
+#Finding Redundant Rules.
+subset.matrix<-is.subset(rules,rules, sparse = FALSE)
+#subset.matrix <- is.subset(rules.sorted, rules.sorted, sparse = FALSE)
+subset.matrix[lower.tri(subset.matrix,diag = T)]<-NA
+redundant<-colSums(subset.matrix,na.rm = T)>=1.65
+which(redundant)
+
+#Removing Redundant Rules
+rules.pruned<-rules[!redundant]
+rules.pruned<-sort(rules.pruned, by='lift')
+inspect(rules.pruned)
+plot(rules.pruned)
+plot(rules.pruned, method = 'grouped')
---
+++ Movie1.R
@@ -0,0 +1,58 @@
+library(arules)
+library(arulesViz)
+
+mov <- read.csv(file.choose(), header = T, colClasses = 'factor')
+movi <- mov[,6:15]
+
+movi$Sixth.Sense<-as.factor(movi$Sixth.Sense)
+movi$Gladiator<-as.factor(movi$Gladiator)
+movi$LOTR1<-as.factor(movi$LOTR1)
+movi$Harry.Potter1<-as.factor(movi$Harry.Potter1)
+movi$Patriot<-as.factor(movi$Patriot)
+movi$LOTR2<-as.factor(movi$LOTR2)
+movi$Harry.Potter2<-as.factor(movi$Harry.Potter2)
+movi$Braveheart<-as.factor(movi$Braveheart)
+movi$Green.Mile<-as.factor(movi$Green.Mile)
+
+str(movi)
+
+rules<-apriori(movi)
+rules.sorted<-sort(rules, by = 'lift')
+
+plot(rules, jitter = 0)
+plot(rules, method = 'grouped')
+
+summary(rules)
+summary(movi)
+
+# rules with rhs containing Patriot only
+rules = apriori(movi,parameter = list(minlen = 1,supp = 0.1,conf = 0.8),appearance = list(rhs = c("Patriot=1"), lhs = c("Sixth.Sense=1",'Gladiator=1','LOTR1=1','Harry.Potter1=1','LOTR2=1','Harry.Potter2=1','LOTR=1','Braveheart=1','Green.Mile=1')))
+plot(rules)                                                                                          
+inspect(rules) 
+
+# rules with rhs containing Sixth Sense only
+rules = apriori(movi,parameter = list(minlen = 1,supp = 0.1,conf = 0.8),appearance = list(rhs = c("Sixth.Sense=1"), lhs = c("Patriot=1",'Gladiator=1','LOTR1=1','Harry.Potter1=1','LOTR2=1','Harry.Potter2=1','LOTR=1','Braveheart=1','Green.Mile=1')))
+plot(rules)                                                                                          
+inspect(rules)
+
+# rules with rhs containing Patriot and Sixth Sense both
+rules = apriori(movi,parameter = list(minlen = 1,supp = 0.1,conf = 0.8),appearance = list(rhs = c("Patriot=1","Sixth.Sense=1"), lhs = c('Gladiator=1','LOTR1=1','Harry.Potter1=1','LOTR2=1','Harry.Potter2=1','LOTR=1','Braveheart=1','Green.Mile=1')))
+plot(rules)                                                                                          
+inspect(rules)
+
+#Finding Redundant Rules.
+subset.matrix<-is.subset(rules,rules, sparse = FALSE)
+#subset.matrix <- is.subset(rules.sorted, rules.sorted, sparse = FALSE)
+subset.matrix[lower.tri(subset.matrix,diag = T)]<-NA
+redundant<-colSums(subset.matrix,na.rm = T)>=1.66
+which(redundant)
+
+#Removing Redundant Rules
+rules.pruned<-rules[!redundant]
+rules.pruned<-sort(rules.pruned, by='lift')
+inspect(rules.pruned)
+plot(rules.pruned)
+plot(rules.pruned, method = 'grouped')
+plot(rules,method = 'graph')
+
+
