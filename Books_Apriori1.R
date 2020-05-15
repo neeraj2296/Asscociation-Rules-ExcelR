07e76ee129af3d2e63b7ddae79810013f9f39c22
Neeraj Kumar S J <neerajkumarsj96@gmail.com>
2020-05-15 22:43:17 +0530

    Update Books_Apriori1.R

--- Books_Apriori1.R
+++ Books_Apriori1.R
@@ -1,8 +1,10 @@
+#Including the necassary Libraries
 library(arules)
-#install.packages("arulesViz")
 library(arulesViz)
 
+#Loading the data set
 book <- read.csv(file.choose())
+#Factorising the data for better classification
 book$ChildBks<-as.factor(book$ChildBks)
 book$YouthBks<-as.factor(book$YouthBks)
 book$CookBks<-as.factor(book$CookBks)
@@ -17,27 +19,27 @@ book$Florence<-as.factor(book$Florence)
 #rules$conf
 str(book)
 
+#Applying the Apriori Alogithm and figuring out rules
 rules = apriori(book)
 arules::inspect(rule)
 rules.sorted<-sort(rules, by = 'lift')
 arules::inspect(rules)
 
+#Visualising the rules
 plot(rules)
+summary(book)
 
-summary(book)
-summary(book)
 # rules with rhs containing CookBks only
 rules = apriori(book,parameter = list(minlen = 1,supp = 0.11,conf = 0.5),appearance = list(rhs = "CookBks=1"))
 plot(rules, jitter = 0)
 
+#Summarised view f rules with rhs having CookBks only
 summary(rules)
 arules::inspect(rules)
-?apriori          
-
-summary(rules)
-
+?apriori
 inspect(rules)
-# rules with rhs containing CookBks only
+
+# rules with rhs containing CookBks only & with all others taken as bought( i.e. 1)
 rules = apriori(book,parameter = list(minlen = 1,supp = 0.1,conf = 0.3),appearance = list(rhs = c("CookBks=1"),lhs = c("RefBks=1","ArtBks=1","YouthBks=1","GeogBks=1","DoItYBks=1","ChildBks=1","ItalArt=1","ItalAtlas=1","ItalCook=1"), default ="none"))
 plot(rules, jitter = 0)
 #plot(rules,method='grouped')
@@ -45,11 +47,8 @@ plot(rules, jitter = 0)
 summary(rules)
 arules::inspect(rules)
 ?apriori          
-
 summary(rules)
-
 inspect(rules)
-
 
 #Finding Redundant Rules.
 subset.matrix<-is.subset(rules,rules, sparse = FALSE)
